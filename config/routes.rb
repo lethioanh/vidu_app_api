7aaf2c99ccbc2387f4faed4ff73fd4e97af95408
at-oanhlt <Oanhlt@asiantech.vn>
2016-08-12 14:25:28 +0700

    the first

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  post '/signup', to: 'users#create'
+  get '/signup', to: 'users#new'
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
+  get '/home', to: 'static_pages#home'
+  get '/help', to: 'static_pages#help'
+  root 'static_pages#home'
+  resources :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
